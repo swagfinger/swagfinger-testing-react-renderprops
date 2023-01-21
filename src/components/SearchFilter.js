1ff2f13b80f9ef3a639a104cbb84d69573699f43
swagfinger <swagfinger@gmail.com>
2023-01-21 16:58:01 +0800

    add comments

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,6 +1,17 @@
-export const SearchFilter = ({ render, filterMethod, query, data }) => {
-  //order is important
-  const results = filterMethod(query, data);
+import { useState } from 'react';
 
-  return render({ results });
+export const SearchFilter = ({ render, filterMethod, data }) => {
+  //handles state of input
+  const [queryString, setQueryString] = useState('');
+
+  //and when input changes...
+  const onChangeHandler = (e) => {
+    console.log('console: ', e.currentTarget.value);
+    setQueryString(e.currentTarget.value);
+  };
+
+  //order is important
+  const results = filterMethod(queryString, data);
+
+  return render({ results, onChangeHandler, queryString });
 };
--- src/components/SearchFilterExample.js
+++ src/components/SearchFilterExample.js
@@ -1,41 +1,39 @@
-import { useState } from 'react';
-
 import { SearchFilter } from './SearchFilter';
 
+//SearchFilter needs 2 things
+//1. data
+//2. filter function
+//3. render={} which gets stuff out from inside SearchFilter
 export const SearchFilterExample = () => {
-  const [queryString, setQueryString] = useState('');
-
   const data = ['pineapple', 'banana', 'apple'];
 
+  //what to do with data - its external because we can change out the filter function as we please.
+  //else we have to have many search filters for each filterable data, now, we can have many filter methods using the same SearchFilter
   const filterMethod = (query, data) => {
     const filtered = query.length
       ? data.filter((item) => {
           return item.includes(query.toLowerCase());
         })
       : [];
-
     return filtered;
   };
 
-  const onChangeHandler = (e) => {
-    console.log('console: ', e.currentTarget.value);
-    setQueryString(e.currentTarget.value);
-  };
-
   return (
-    <>
-      <input onChange={onChangeHandler} value={queryString} />
-
-      <SearchFilter
-        filterMethod={filterMethod}
-        data={data}
-        query={queryString}
-        render={({ results }) => {
-          return results.map((item, index) => {
-            return <div key={index}>{item}</div>;
-          });
-        }}
-      />
-    </>
+    <SearchFilter
+      filterMethod={filterMethod}
+      data={data}
+      render={({ results, onChangeHandler, queryString }) => {
+        return (
+          <>
+            {/* separation of concerns - input and filter is not the same thing... 
+            //thats why its not baked inside a SearchFilter which is a helper function */}
+            <input onChange={onChangeHandler} value={queryString} />
+            {results.map((item, index) => {
+              return <div key={index}>{item}</div>;
+            })}
+          </>
+        );
+      }}
+    />
   );
 };
